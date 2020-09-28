6c4b93fe2b0f17da3fe17c6a97f9b19e3f6861e4
Ashul <anshulupsc@gmail.com>
2020-09-29 00:55:42 +0530

    Create add-bill.js

---
+++ models/add-bill.js
@@ -0,0 +1,24 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+
+const addBillSchema = new Schema({
+
+    full_name:{type:String}, 
+    mobile_no: {type:String},
+    email_id: {type:String},
+    adress: {type:String},
+    order_id: {type:String},
+    total_amount: {type:String},
+
+    item_name: {type:String},
+    item_weight: {type:String},
+    item_qun: {type:String},
+    item_rate:{type:String},
+    item_amount: {type:String},
+    
+
+    created:{type:Date, default:Date.now}
+})
+
+
+module.exports = mongoose.model('AddBill', addBillSchema)
