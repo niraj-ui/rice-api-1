abc4e51d89ebe030e37412590f62ca2d4fbc6471
Ashul <anshulupsc@gmail.com>
2020-10-11 23:39:47 +0530

    Create krbl-enquiry.js

---
+++ models/krbl-enquiry.js
@@ -0,0 +1,24 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+
+const krblEnquirySchema = new Schema({
+    mobile:{type:String},
+
+    name: {type:String},
+    email: {type:String},
+    area: {type:String},  
+
+    brands: {type:String},
+    invest: {type:String},
+    town: {type:String},
+    state: {type:String},
+    district: {type:String},
+
+    pincode: {type:String},
+    message: {type:String},
+
+    created:{type:Date, default:Date.now}
+})
+
+
+module.exports = mongoose.model('krblEnuiry', krblEnquirySchema)
