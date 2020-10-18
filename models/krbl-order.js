7db5dad09238b1aff5d6a2634c45687d8e0ee7c5
Ashul <anshulupsc@gmail.com>
2020-10-18 23:19:00 +0530

    Create krbl-order.js

---
+++ models/krbl-order.js
@@ -0,0 +1,18 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema;
+
+const krblOrderSchema = new Schema({
+   
+    name:{type:String},
+    email:{type:String},
+    mobile:{type:String},
+    address:{type:String},
+    city:{type:String},
+    postcode:{type:String},
+    message:{type:String},
+
+    created:{type:Date, default:Date.now}
+})
+
+
+module.exports = mongoose.model('krblOrder', krblOrderSchema)
