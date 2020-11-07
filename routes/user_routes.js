df0926bef2af79234334b2508231e764716f4a4b
Ashul <anshulupsc@gmail.com>
2020-11-07 13:56:25 +0530

    Update user_routes.js

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+var nodemailer = require('nodemailer');
 const User = require('../models/user')
 const Contact = require('../models/contact')
 const TrackOrder = require('../models/track-order')
@@ -238,13 +239,36 @@ router.put('/add-bill/:id', (req,res)=>{
         }
     })
 })
-
+// ------------------   -----------     mail send --------------    //
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: 'nirajbca2011@gmail.com',
+      pass: 'Sonu@1703'
+    }
+  });
+  
+  var mailOptions = {
+    from: 'nirajbca2011@gmail.com',
+    to: 'nirajbca2011@gmail.com',
+    subject: 'Sending Email using Node.js',
+    text: `Hi Smartherd, thank you for your nice Node.js tutorials.
+            I will donate 50$ for this course. Please send me payment options.`
+    // html: '<h1>Hi Smartherd</h1><p>Your Messsage</p>'        
+  };
+  
 
 
 // krbl start here -----------------------------------------------------------------
 
 router.post('/krbl-enquiry', (req, res)=>{
-  
+      transporter.sendMail(mailOptions, function(error, info){
+        if (error) {
+          console.log(error);
+        } else {
+          console.log('Email sent: ' + info.response);
+        }
+      });
 let newtrackC = new krblEnuiry({
     mobile:req.body.mobile,
 
