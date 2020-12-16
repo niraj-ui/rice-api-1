c64ca096e5fe45aa7f5e8ab3422300b61bc46379
Ashul <anshulupsc@gmail.com>
2020-12-16 16:56:34 +0530

    Update ifco-order.js

--- models/ifco-order.js
+++ models/ifco-order.js
@@ -1,6 +1,13 @@
 const mongoose = require ('mongoose');
 const Schema = mongoose.Schema;
 
+const productSchema = new Schema({
+    item_name:{type:String},
+    item_weight:{type:String},
+    item_qun:{type:String},
+    item_amount:{type:String},
+    item_rate:{type:String},   
+})
 const ifcoOrderSchema = new Schema({
    
     name:{type:String},
@@ -10,6 +17,11 @@ const ifcoOrderSchema = new Schema({
     city:{type:String},
     postcode:{type:String},
     message:{type:String},
+   
+    order_id: {type:String},
+    total_amount: {type:String},
+    item_paid: {type:Boolean},
+    product: [productSchema],
 
     created:{type:Date, default:Date.now}
 })
