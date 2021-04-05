217f2356cc4562322bc7369db28c17629f8cfb78
Ashul <anshulupsc@gmail.com>
2021-04-06 01:01:17 +0530

    Update krbl-order.js

--- models/krbl-order.js
+++ models/krbl-order.js
@@ -3,19 +3,15 @@ const Schema = mongoose.Schema;
 
 const krblOrderSchema = new Schema({
    
-    name:{type:String},
-    f_name:{type:String},
+      name:{type:String},
     email:{type:String},
     mobile:{type:String},
-    
-    area :{type:String},
-    invest :{type:String},
-    disctrict:{type:String},
-    state :{type:String},
-    town :{type:String},
-   
+    address:{type:String},
+    city:{type:String},
     postcode:{type:String},
     message:{type:String},
+
+    
 
     created:{type:Date, default:Date.now}
 })
